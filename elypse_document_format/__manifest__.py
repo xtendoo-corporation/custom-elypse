d20dcdf7fa3cd9a8f3cf6105dcb808f3ea4f1ac1
Dani <dani.xtendoo@gmail.com>
2024-01-19 14:05:46 +0100

    [ADD], elypse_document_format

---
+++ elypse_document_format/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Elypse Document Format",
+    "summary": """Formatos de documentos entregados por Xtendoo""",
+    "version": "16.1.0.0",
+    "description": """Formatos de documentos entregados por Xtendoo""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        "base",
+
+    ],
+    "data": [
+        "views/invoice/invoice_document.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
